265d91d6a8b289f3a753babda108f28887c4d6ec
elmerbool <35117496+elmerbool@users.noreply.github.com>
2020-09-12 13:08:03 +1000

    Refactored EventStoreCoreClient.

---
+++ src/main/IEventStoreCoreClient.cs
@@ -0,0 +1,16 @@
+﻿using ei8.EventSourcing.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ei8.EventSourcing.Client
+{
+    public interface IEventStoreCoreClient
+    {
+        Task<IEnumerable<Notification>> Get(string outBaseUrl, Guid aggregateId, int fromVersion, CancellationToken cancellationToken = default);
+
+        Task Save(string inBaseUrl, IEnumerable<Notification> notifications, CancellationToken cancellationToken = default);
+    }
+}
